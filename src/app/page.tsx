eb8e70d72c2bf4c69009ecaf5865a3d6f8ea3515
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-07-26 17:15:46 +0700

    CategoryList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,10 @@ const HomePage = () => {
 				<ProductList />
 			</div>
 
-			<div className='mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64'>
-				<h1 className='text-2xl'>Categories</h1>
+			<div className='mt-24'>
+				<h1 className='text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12'>
+					Categories
+				</h1>
 				<CategoryList />
 			</div>
 
--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,5 +1,46 @@
-import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function CategoryList() {
-	return <div>CategoryList</div>
+	return (
+		<div className='px-4 overflow-x-scroll scrollbar-hide'>
+			<div className='flex gap-4 md:gap-8'>
+				<Link
+					href={`/list?cat=test`}
+					className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'
+				>
+					<div className='relative bg-slate-100 w-full h-96'>
+						<Image
+							src='https://images.pexels.com/photos/5707554/pexels-photo-5707554.jpeg'
+							alt=''
+							fill
+							sizes='20vw'
+							className='object-cover'
+						/>
+					</div>
+					<h1 className='mt-8 font-light text-xl tracking-wide'>
+						Category Name
+					</h1>
+				</Link>
+
+				<Link
+					href={`/list?cat=test`}
+					className='flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6'
+				>
+					<div className='relative bg-slate-100 w-full h-96'>
+						<Image
+							src='https://images.pexels.com/photos/5707554/pexels-photo-5707554.jpeg'
+							alt=''
+							fill
+							sizes='20vw'
+							className='object-cover'
+						/>
+					</div>
+					<h1 className='mt-8 font-light text-xl tracking-wide'>
+						Category Name
+					</h1>
+				</Link>
+			</div>
+		</div>
+	)
 }
